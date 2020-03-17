de5b8c879255945a40b273d54fc22675dffc2f1c
nudelx <nudelx@gmail.com>
2020-03-17 15:42:00 +0200

    done

--- src/covidBoard.js
+++ src/covidBoard.js
@@ -1,10 +1,10 @@
 import React, { useEffect, useState } from 'react'
 import Card from './card'
+import useTimer from './useTimer'
 
 export default () => {
   const [cards, setCards] = useState([])
-
-  useEffect(() => {
+  const getData = () =>
     fetch('https://coronavirus-19-api.herokuapp.com/countries/')
       .then(res => res.json())
       .then(res =>
@@ -14,10 +14,15 @@ export default () => {
           })
         )
       )
-  }, [])
+  const timer = useTimer(14400000) // 4 hours
+
+  useEffect(() => {
+    getData()
+  }, [timer])
 
   return (
     <div>
+      <div> Covid-19 Live Dashboard</div>
       {cards.map((c, index) => (
         <Card key={c.country} {...c} index={index + 1} />
       ))}
