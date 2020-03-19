0d3c091c201fcc748b1ef1119aca1e54f1bcf073
nudelx <nudelx@gmail.com>
2020-03-19 13:30:50 +0200

    new deploy Thu Mar 19 13:30:50 IST 2020

---
+++ precache-manifest.5a42a74194bb556d382a238b5de572de.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e697e6b8dd361043cde20812f9d6230b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d9e2e691469b28d9662a",
+    "url": "/static/css/main.6866901a.chunk.css"
+  },
+  {
+    "revision": "f843edfc4483a7a3ce74",
+    "url": "/static/js/2.82fa0618.chunk.js"
+  },
+  {
+    "revision": "41b0c3da6a8866c17968078c7c246d41",
+    "url": "/static/js/2.82fa0618.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d9e2e691469b28d9662a",
+    "url": "/static/js/main.7a4ab6b9.chunk.js"
+  },
+  {
+    "revision": "3a90206e803d5eae6272",
+    "url": "/static/js/runtime-main.78cfa4c0.js"
+  }
+]);
