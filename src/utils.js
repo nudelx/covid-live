7a56ca1ddd40e4b937ce01b07f1e3a2def7eef0d
nudelx <nudelx@gmail.com>
2020-04-10 18:31:25 +0300

    clean

---
+++ src/utils.js
@@ -0,0 +1,24 @@
+export const exclude = [
+  'europe',
+  'asia',
+  'north america',
+  'south america',
+  'total:'
+]
+export const filterCards = setWorld => res => {
+  return res.filter(c => {
+    if (c.country.toLowerCase() === 'world') {
+      typeof setWorld === 'function' && setWorld(c)
+      return false
+    }
+    if (c.country.length === 0 || exclude.includes(c.country.toLowerCase())) {
+      console.log(c.country)
+      return false
+    }
+    return true
+  })
+}
+
+export const handleError = setError => error => {
+  console.log(error.toString()) || setError('The API temporary unavailable ')
+}
