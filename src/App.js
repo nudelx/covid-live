cabbaee8d69f9a823820c633f0b54e528acc4a39
nudelx <nudelx@gmail.com>
2020-04-17 16:26:26 +0300

    done with storage refresh

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React from 'react'
 import './App.css'
 import CovidBoard from './components/covidBoard'
+import { localStorageRefresh } from './utils/localStorageToken'
+
+localStorageRefresh('bee66946449a')
 
 function App() {
   return (
