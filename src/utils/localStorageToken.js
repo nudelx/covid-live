26c3a1d1fbb342ebf2b3aca583526f6c1c48eb98
nudelx <nudelx@gmail.com>
2020-04-17 16:27:18 +0300

    done with storage refresh

--- src/utils/localStorageToken.js
+++ src/utils/localStorageToken.js
@@ -2,7 +2,6 @@ export const localStorageRefresh = (token) => {
   const tokenName = 'covid19token'
   const localToken = localStorage.getItem('covid19token')
   if (localToken !== token) {
-    debugger
     localStorage.clear()
     localStorage.setItem(tokenName, token)
   }
