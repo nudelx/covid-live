621254e97f1fa06bbd0eee2e71334a7549054b55
nudelx <nudelx@gmail.com>
2020-04-27 09:44:15 +0300

    abs

--- src/components/stats.js
+++ src/components/stats.js
@@ -5,7 +5,7 @@ export default ({ stats }) => {
   return (
     <div className={`stats ${stats > 0 ? 's-red' : 's-green'}`}>
       <div className={stats > 0 ? 'arrow-up' : 'arrow-down'}></div>
-      {`${stats ? stats.toFixed(2) + '%' : 0}`}
+      {`${stats ? Math.abs(stats).toFixed(2) + '%' : 0}`}
     </div>
   )
 }
