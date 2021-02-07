56af052d87c1ff64d139356bbdc523018fff7b51
nudelx <nudelx@gmail.com>
2021-02-07 16:52:25 +0200

    new deploy Sun Feb  7 16:52:25 IST 2021

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.aa22a18ed9a979674e0df26cbb25fd9d.js"
+  "/precache-manifest.d03aeb9a5a6676b62b7bb33330c27fbe.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
